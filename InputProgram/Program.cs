48e664c2ebf24b88aed6494a90b68e9032ae5fe2
Lance Hancock <lancehancockac1@gmail.com>
2022-07-28 09:33:57 +0200

    Add project files.

---
+++ InputProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Taking user input
+Console.WriteLine("Enter your name below:");
+String name = Console.ReadLine();
+Console.WriteLine("Hello " + name);
+//Taking user input and converting it
+Console.WriteLine("Enter your age below:");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your age is " + age);
+Console.ReadKey();
